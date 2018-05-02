5d93810c37d3df8ed63a1d633309929f6b3c18f4
StJacquesJeannine <jmcheval@sympatico.ca>
2018-05-02 11:50:39 -0400

    Last bit?

---
+++ R/f.R
@@ -0,0 +1,14 @@
+#' Title
+#'  This is a big mess
+#' @param x This is the first argument
+#' @param y This is the second argument
+#'
+#' @return   Something I hope
+#' @export
+#'
+#' @examples
+#' f(2,3)
+#'
+f <- function(x , y) {
+  x + y
+}
